b156634c9c49eb0fb36d71764daa2db9d12e4e1b
James Kapella <james.kapella1@OVO-WL6YLWF7L5.local>
2022-09-08 16:21:09 +0100

    adding seeding example

--- seeds/index.js
+++ seeds/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cities = require('./cities')
 const { places, descriptors } = require('./seedHelper')
 const Campground = require('../models/campground');
+const User = require('../models/user');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -19,11 +20,24 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     // const c = new Campground({ title: 'Purple Field' });
     // await c.save();
+
+    //CREATE A USER
+    const user = new User(  {
+        email: 'daveyD@mail.com',
+        username: 'daveyD',
+        salt: 'nonsense',
+        hash: 'fakehash',
+      });
+    //SAVE USER
+    await user.save();
+    //GET USER ID
+    const myuser = await find.user();
+
     for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
-            author: '630340045e4ce3059829ac9f',
+            author: 'myuser.id',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Perspiciatis optio recusandae architecto error aperiam dolorum consequuntur, tenetur vero animi in similique, voluptas, autem necessitatibus nam magni incidunt ullam? Nesciunt, expedita?',
